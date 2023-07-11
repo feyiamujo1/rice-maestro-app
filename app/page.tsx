d9c7fa9bc269a30a72dec024cdf257d6c0aff6a8
Joseph Odunsi <odunsiolakunbi@gmail.com>
2023-07-11 21:06:33 +0100

    remove Image

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,16 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
     <div className="p-0 m-0 w-full relative">
       <nav className="bg-white w-full absolute left-0 right-0 top-0 z-50 py-2 shadow-md">
         <div className="w-11/12 mx-auto">
           <div className="flex items-center gap-0.5">
-            <Image
+            {/* <Image
               width={70}
               height={60}
               src="/images/wheat-14.jpg"
               className=" object-fill"
               alt="logo"
-            />
+            /> */}
             <p className="flex flex-col -space-y-1 text-[#2c963f]">
               <span>Rice</span>
               <span>Maestro</span>
