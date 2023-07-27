4e6524b8776dfd1e4a67e4e2885bb2337a78aa73
feyiamujo1 <feyisayoamujoyegbe@gmail.com>
2023-07-27 12:39:10 +0100

    Change the color of the mark as red button

--- components/NotificationCard.tsx
+++ components/NotificationCard.tsx
@@ -55,7 +55,7 @@ export function NotificationCard({
                     : `/${notification.slug}`
                 }
               >
-                <span className="flex h-2 w-2 translate-y-1 rounded-full bg-custom-green" />
+                <span className="flex h-2 w-2 translate-y-1 rounded-full bg-[#999999]" />
                 <div className="space-y-1">
                   <p className="text-sm font-medium leading-none">
                     {notification.source + " was " + notification.action + "d"}
